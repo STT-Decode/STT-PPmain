fde0466dc0afee8ab9139be7703969619fdecc92
FTC computer links <ftcscw@gmail.com>
2025-10-06 13:43:39 +0200

    pedro is making me cry (its gonna work when im finished tuning tho)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -19,12 +19,9 @@ public class Constants {
 
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(4)
-            .forwardZeroPowerAcceleration(-41.278)
-            .lateralZeroPowerAcceleration(-59.7819)
             .useSecondaryTranslationalPIDF(false)
             .useSecondaryHeadingPIDF(false)
             .useSecondaryDrivePIDF(false)
-            .centripetalScaling(0.0005)
             .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0))
             .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0))
             .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1, 0, 0, 0.6, 0));
@@ -34,20 +31,20 @@ public class Constants {
             .leftRearMotorName("left_back")
             .rightFrontMotorName("right_front")
             .rightRearMotorName("right_back")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(57.8741)
+            .xVelocity(53)
             .yVelocity(52.295);
 
     public static OTOSConstants localizerConstants = new OTOSConstants()
             .hardwareMapName("sensor_otos")
             .linearUnit(DistanceUnit.INCH)
-            .angleUnit(AngleUnit.DEGREES)
+            .angleUnit(AngleUnit.RADIANS)
             .offset(new SparkFunOTOS.Pose2D(0, 13, Math.PI / 2))
-            .linearScalar(9)
-            .angularScalar(0.0017);
+            .linearScalar(1.07)
+            .angularScalar(0.97);
 
     public static PathConstraints pathConstraints = new PathConstraints(
             0.99,
