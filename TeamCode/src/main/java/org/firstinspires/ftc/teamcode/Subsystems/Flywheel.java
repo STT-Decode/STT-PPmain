d24c744dd5ae6b4c2f181f3e3dbd834a6293424c
pc links <ftcscw@gmail.com>
2025-11-25 14:35:17 +0100

    Started working on flywheel velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Flywheel.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
 import dev.nextftc.core.commands.groups.ParallelGroup;
-import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.controllable.RunToVelocity;
@@ -13,27 +13,32 @@ import dev.nextftc.hardware.powerable.SetPower;
 public class Flywheel implements Subsystem
 {
     public static final Flywheel INSTANCE = new Flywheel();
+    private Flywheel() { }
 
-    private Flywheel()
+    private MotorEx flywheel1 = new MotorEx("flywheel1");
+    private MotorEx flywheel2 = new MotorEx("flywheel2").reversed();
+
+    private ControlSystem controlSystem = ControlSystem.builder().posPid(0, 0, 0)
+        .build();
+
+
+    @Override
+    public void periodic()
     {
+        flywheel1.setPower(controlSystem.calculate(flywheel1.getState()));
+        flywheel2.setPower(controlSystem.calculate(flywheel2.getState()));
+        ActiveOpMode.telemetry().addData("debug", controlSystem.calculate(flywheel2.getState()));
+        ActiveOpMode.updateTelemetry(ActiveOpMode.telemetry());
     }
-
-    private final MotorEx flywheel1 = new MotorEx("flywheel1");
-    private final MotorEx flywheel2 = new MotorEx("flywheel2").reversed();
 
     public Command turnOn()
     {
-        return new ParallelGroup(
-                new SetPower(flywheel1, -0.965).requires(this),
-                new SetPower(flywheel2, -0.965).requires(this));
+        return new RunToVelocity(controlSystem, 2400, 100).requires(this);
     }
-
 
     public Command turnOff()
     {
-        return new ParallelGroup(
-                new SetPower(flywheel1, 0).requires(this),
-                new SetPower(flywheel2, 0).requires(this));
+        return new RunToVelocity(controlSystem, 0).requires(this);
     }
 
     public Command setCustomPower(double power)
